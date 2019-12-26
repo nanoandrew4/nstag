87184f33596de46383dd171bdf6ff0d16d9c8904
nanoandrew4 <nanoandrew4@gmail.com>
2019-12-26 16:30:35 +0100

    Changed CLIParser to exit with error codes if the command parsing fails, instead of returning to main which always ends in exit code 0

--- src/main/java/nsteg/main/CLIParser.java
+++ src/main/java/nsteg/main/CLIParser.java
@@ -26,10 +26,12 @@ public class CLIParser {
 			encode(args);
 		else if (args.length >= 5 && "-d".equals(args[0]))
 			decode(args);
-		else
+		else {
 			System.err.println(
 					"Input is not valid. For help on running the program from the command line, use the --help flag"
-			);
+							  );
+			System.exit(1);
+		}
 	}
 
 	// Designed to fit perfectly in a standard 80x24 Linux terminal window
@@ -58,61 +60,61 @@ public class CLIParser {
 				"\t       used to hide the data.\n" +
 				"\t       Supported image codecs: " + supportedInImgFormats + "\n" +
 				"\t       Supported audio codecs: " + supportedInAudFormats + "\n"
-		);
+						  );
 		System.out.println(
 				"\t--hf:  File(s) to hide. To encode (hide) multiple files, delimit the\n" +
 				"\t       files with commas, and do not use spaces between filenames or \n" +
 				"\t       commas. Eg: \"test.txt,test.pdf,test.ppt\". The file names will\n" +
 				"\t       be stored alongside the data, and used during the decoding process.\n"
-		);
+						  );
 		System.out.println(
 				"\t--mof: Media output file. The media file with the encoded data will be\n" +
 				"\t       saved under this name. Must be a supported file type, and a\n" +
 				"\t       lossless codec.\n" +
 				"\t       Supported image codecs: " + supportedOutImgFormats + "\n" +
 				"\t       Supported audio codecs: " + supportedOutAudFormats + "\n"
-		);
+						  );
 		System.out.println(
 				"\t--lsb: Number of least significant bits to use for encoding. A larger\n" +
 				"\t       number will allow more data to fit, but cause more distortion.\n" +
 				"\t       Valid range is 1-8, both inclusive.\n"
-		);
+						  );
 		System.out.println("Decode arguments:");
 		System.out.println(
 				"\t--mif: Media input file. This is the image or audio file from which\n" +
 				"\t       nsteg will attempt to decode files.\n" +
 				"\t       Supported image codecs: " + supportedOutImgFormats + "\n" +
 				"\t       Supported audio codecs: " + supportedOutAudFormats + "\n"
-		);
+						  );
 		System.out.println("Optional arguments:");
 		System.out.println(
 				"\t--enc:  Valid values are \"true\" and \"false\". Used to determine whether\n" +
 				"\t        encryption will be used when encoding. If this flag is not used,\n" +
 				"\t        the program will ask the user if the data should be encrypted.\n"
-		);
+						  );
 		System.out.println(
 				"\t--pass: Passphrase to encrypt the data with, if encryption is used.\n" +
 				"\t        Setting this field carries a security risk, since the password\n" +
 				"\t        remains in memory longer than if this field was not set, in \n" +
 				"\t        which case the program will prompt it right before it is\n" +
 				"\t        necessary, and wipe it from memory once the encryption is done."
-		);
+						  );
 		System.out.println("--------------------------------------------------------------------------------");
 		System.out.println(
 				"Sample encoding use:\n" +
 				"\t\"java -jar nsteg-vX.Y-release.jar -e --mif test.jpg\n" +
 				"\t --hf test.txt,test.pdf --mof outtest.png --lsb 3\"\n"
-		);
+						  );
 		System.out.println(
 				"Sample decoding use:\n" +
 				"\t\"java -jar nsteg-vX.Y-release.jar -d --mif outtest.png\n" +
 				"\t --hf test.txt,test.pdf\"\n"
-		);
+						  );
 		System.out.println(
 				"Sample encryption use:\n" +
 				"\t\"java -jar nsteg-vX.Y-release.jar -e --mif test.jpg --hf test.txt\n" +
 				"\t --mof outtest.png --lsb 2 --enc true --pass thebestpassword\""
-		);
+						  );
 	}
 
 	/**
@@ -135,7 +137,7 @@ public class CLIParser {
 					|| (fileType == FileType.IMAGE && !Encoder.inImgFormats.contains(fileExt))
 					|| (fileType == FileType.AUDIO && !Encoder.inAudFormats.contains(fileExt))) {
 					System.err.println("Input media file is invalid, please see supported file types. Exiting.");
-					return;
+					System.exit(2);
 				}
 
 				mediaInputFile = args[i + 1];
@@ -145,30 +147,30 @@ public class CLIParser {
 				for (String fName : filesToHide)
 					if (!Files.exists(Paths.get(fName))) {
 						System.err.println("The file \"" + fName + "\" does not exits... Exiting.");
-						return;
+						System.exit(3);
 					}
 			} else if ("--mof".equals(args[i])) {
 				String[] fNameSplit = args[i + 1].split("\\.");
 				String fileExt = fNameSplit[fNameSplit.length - 1];
 				if ((fileType == FileType.AUDIO && !Encoder.outAudFormats.contains(fileExt))
 					|| (fileType == FileType.IMAGE && !Encoder.outImgFormats.contains(fileExt))) {
-					System.err.println("Input type mismatch. Please ensure you encode from an image to an image" +
+					System.err.println("Input type mismatch. Please ensure you encode from an image to an image " +
 									   "or from an audio file to an audio file. Exiting.");
-					return;
+					System.exit(4);
 				}
 
 				mediaOutputFile = args[i + 1];
 			} else if ("--lsb".equals(args[i])) {
 				try {
-					LSBsToUse = Integer.valueOf(args[i + 1]);
+					LSBsToUse = Integer.parseInt(args[i + 1]);
 				} catch (Exception e) {
 					System.err.println("LSB must be a numeric value. Exiting.");
-					return;
+					System.exit(5);
 				}
 
 				if (LSBsToUse < 1 || LSBsToUse > 8) {
 					System.err.println("LSB must be between 1-8. Exiting.");
-					return;
+					System.exit(6);
 				}
 			} else if ("--enc".equals(args[i]))
 				encrypt = Boolean.valueOf(args[i + 1]);
@@ -176,15 +178,17 @@ public class CLIParser {
 				pass = args[i + 1];
 			else {
 				System.err.println("Unknown option \"" + args[i] + "\". Exiting.");
-				return;
+				System.exit(7);
 			}
 		}
 
 		if (mediaInputFile.length() > 0 && mediaOutputFile.length() > 0 && filesToHide.length > 0 && LSBsToUse > 0) {
 			System.out.println();
 			Encoder.encode(mediaInputFile, filesToHide, mediaOutputFile, LSBsToUse, encrypt, pass);
-		} else
+		} else {
 			System.err.println("Arguments missing. For help, please use the --help flag. Exiting.");
+			System.exit(8);
+		}
 	}
 
 	/**
@@ -207,7 +211,7 @@ public class CLIParser {
 					|| (fileType == FileType.IMAGE && !Encoder.outImgFormats.contains(fileExt))
 					|| (fileType == FileType.AUDIO && !Encoder.outAudFormats.contains(fileExt))) {
 					System.err.println("Input media file is invalid, please see supported file types. Exiting.");
-					return;
+					System.exit(9);
 				}
 				encodedInputFile = args[i + 1];
 			} else if ("--enc".equals(args[i]))
@@ -216,14 +220,16 @@ public class CLIParser {
 				pass = args[i + 1];
 			else {
 				System.err.println("Unknown option \"" + args[i] + "\". Exiting.");
-				return;
+				System.exit(10);
 			}
 		}
 
 		if (encodedInputFile.length() > 0) {
 			System.out.println();
 			Decoder.decode(encodedInputFile, encrypt, pass);
-		} else
+		} else {
 			System.err.println("Arguments missing. For help, please use the --help flag. Exiting.");
+			System.exit(11);
+		}
 	}
 }
